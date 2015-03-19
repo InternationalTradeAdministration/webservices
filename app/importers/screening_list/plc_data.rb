8f062bedbd84b1c6f2f16e58dcc522e80f030210
ITA Data Services <DataServices@trade.gov>
2015-03-19 13:54:56 -0700

    Update

--- app/importers/screening_list/plc_data.rb
+++ app/importers/screening_list/plc_data.rb
@@ -3,8 +3,9 @@ module ScreeningList
     include ::Importer
     include ScreeningList::TreasuryListImporter
     self.default_endpoint =
-      'http://www.treasury.gov/resource-center/sanctions/Terrorism-Proliferation-Narcotics/Documents/ns_plc.xml'
+      'http://www.treasury.gov/ofac/downloads/consolidated/consolidated.xml'
     self.source_information_url =
       'http://www.treasury.gov/resource-center/sanctions/Terrorism-Proliferation-Narcotics/Pages/index.aspx'
+    self.program_id = 'NS-PLC'
   end
 end
