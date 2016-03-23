cf52295bdfc92f744b11247ede293c22df5c6471
ITA Data Services <DataServices@trade.gov>
2016-03-23 12:45:59 -0700

    Update

--- app/importers/data_sources/external_mapping_transformation.rb
+++ app/importers/data_sources/external_mapping_transformation.rb
@@ -6,10 +6,22 @@ module DataSources
     end
 
     def transform(value)
-      url = @options[:url].sub('ORIGINAL_VALUE', URI.encode(value))
-      JsonPath.on(json_response_from(url), @options[:result_path]).first
+      transformed_value = nil
+      @options[:urls].each do |hash|
+        transformed_value = transform_value(value, hash)
+        break if transformed_value.present?
+      end
+      transformed_value
+    end
+
+    def transform_value(value, hash)
+      url_template, result_path, multi_value = hash[:url], hash[:result_path], hash[:multi_value]
+      url = url_template.sub('ORIGINAL_VALUE', URI.encode(value))
+      result = JsonPath.on(json_response_from(url), result_path)
+      result = result.first unless multi_value
+      result
     rescue Exception => e
-      Rails.logger.warn "Unable to get mapping from #{url}: #{e.message}"
+      Rails.logger.warn "Unable to get mapping for #{value} from #{url}: #{e.message}"
       nil
     end
 
