9b73ef1407b1c9e37c653cd88680a3fb6e40cc2c
James Isaacs <james@govwizely.com>
2019-10-30 16:49:21 -0400

    El importer update - ignore empty rows

--- app/importers/screening_list/el_data.rb
+++ app/importers/screening_list/el_data.rb
@@ -36,7 +36,8 @@ module ScreeningList
     }
 
     def import
-      rows = CSV.parse(loaded_resource, headers: true, header_converters: [:symbol], encoding: 'UTF-8')
+      rows = CSV.parse(loaded_resource, headers: true, header_converters: [:symbol],
+                       encoding: 'UTF-8').delete_if { |row| row.to_hash.values.all?(&:blank?) }
       ensure_expected_headers(rows.first)
       @source_list_url = 'http://www.bis.doc.gov/index.php/policy-guidance/lists-of-parties-of-concern/entity-list'
       docs = group_rows(rows).map { |id, grouped| process_grouped_rows(id, grouped) }
